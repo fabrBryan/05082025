6b282c7de2f454fb933f2b29f1814bfe8f678b99
Fabryan <ahmad_pradana_34rpl@student.smktelkom-mlg.sch.id>
2025-08-05 12:02:05 +0700

    Add files via upload

---
+++ dataTeman.java
@@ -0,0 +1,89 @@
+
+
+public class dataTeman {
+    public static void main(String[] args) {
+        // tipe_data_nama_variabel = "nilai";
+        String nama1 = "Elzhar Aararya Adam";
+        int usia1 = 16;
+        String kelas1 = "X RPL 7 SMK TELKOM MALANG";
+        String sekolah1 = "SMPN 1 GIRI BANYUWANGI";
+        String hobi1= "masak";
+
+        String nama2 = "Fildzah Lail Azizi";
+        int usia2 = 16;
+        String kelas2 = "X RPL 7 SMK TELKOM MALANG";
+        String sekolah2 = "SMPN 1 PRIGEN";
+        String hobi2 = "nonton drakor";
+
+        String nama3 = "Grisli Oscar Twi Billywanto";
+        int usia3 = 16;
+        String kelas3 = "X RPL 7 SMK TELKOM MALANG";
+        String sekolah3 = "SMPN 4 KEPANJEN";
+        String hobi3 = "belajar";
+
+        String nama4 = "M Rohman Nur Muhammad";
+        int usia4 = 15;
+        String kelas4 = "X RPL 7 SMK TELKOM MALANG";
+        String sekolah4 = "SMPN 2 SUMBERPUCUNG";
+        String hobi4 = "lari";
+
+        String nama5 = "Abdurrahman Al Ghazi";
+        int usia5 = 15;
+        String kelas5 = "X RPL 7 SMK TELKOM MALANG";
+        String sekolah5 = "SMP INSAN PERMATA MALANG";
+        String hobi5 = "olahraga";
+
+        String nama6 = "Anggita Dewi Madhina";
+        int usia6 = 16;
+        String kelas6 = "X RPL 7 SMK TELKOM MALANG";
+        String sekolah6 = "SMP MA'ARIF NU PANDAAN";
+        String hobi6 = "mendengarkan musik ";
+
+        System.out.println("Nama: " + nama1);
+        System.out.println("Usia: " + usia1 + " tahun");
+        System.out.println("Kelas: " + kelas1);
+        System.out.println("Sekolah: " + sekolah1);
+        System.out.println("Hobi: " + hobi1);
+        System.out.println();
+
+        System.out.println("Nama: " + nama2);
+        System.out.println("Usia: " + usia2 + " tahun");
+        System.out.println("Kelas: " + kelas2);
+        System.out.println("Sekolah: " + sekolah2);
+        System.out.println("Hobi: " + hobi2);
+        System.out.println();
+
+        System.out.println("Nama: " + nama3);
+        System.out.println("Usia: " + usia3 + " tahun");
+        System.out.println("Kelas: " + kelas3);
+        System.out.println("Sekolah: " + sekolah3);
+        System.out.println("Hobi: " + hobi3);
+        System.out.println();
+
+        System.out.println("Nama: " + nama4);
+        System.out.println("Usia: " + usia4 + " tahun");
+        System.out.println("Kelas: " + kelas4);
+        System.out.println("Sekolah: " + sekolah4);
+        System.out.println("Hobi: " + hobi4);
+        System.out.println();
+
+        System.out.println("Nama: " + nama5);
+        System.out.println("Usia: " + usia5 + " tahun");
+        System.out.println("Kelas: " + kelas5);
+        System.out.println("Sekolah: " + sekolah5);
+        System.out.println("Hobi: " + hobi5);
+        System.out.println();
+
+        System.out.println("Nama: " + nama6);
+        System.out.println("Usia: " + usia6 + " tahun");
+        System.out.println("Kelas: " + kelas6);
+        System.out.println("Sekolah: " + sekolah6);
+        System.out.println("Hobi: " + hobi6);
+        System.out.println();
+
+
+    }
+}
+    
+    
+
